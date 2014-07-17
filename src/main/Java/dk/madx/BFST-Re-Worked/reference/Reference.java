ed9ee9ff64e9dddb05d910fcd6be77319735c3c0
Madx-com <Madx.com@gmail.com>
2014-07-18 00:02:07 +0200

    Setting up some basic gradle build script.
    Signed-off-by: Madx-com <Madx.com@gmail.com>

--- src/main/Java/dk/madx/BFST-Re-Worked/reference/Reference.java
+++ src/main/Java/dk/madx/BFST-Re-Worked/reference/Reference.java
@@ -1,7 +1,7 @@
-public class REFERENCE {
+public class Reference {
 
 public static final String Resource_Path = "res";
-public static final String File_Seperator = System..getProperty("file.separator");
+public static final String File_Seperator = System.getProperty("file.separator");
 public static final String System_Dir = System.getProperty("user.dir");
 
 }
